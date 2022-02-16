66ffc7c2c8783f978ef7c3a71fa23d3fc6b6ab93
DeveloperRooney <han_0427@naver.com>
2022-02-16 10:56:03 +0900

    @MockBean을 이용한 가짜 객체 테스트

--- eatgo-api/src/test/java/com/hancoding/eatgo/web/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/com/hancoding/eatgo/web/RestaurantControllerTest.java
@@ -1,20 +1,22 @@
 package com.hancoding.eatgo.web;
 
-import com.hancoding.eatgo.domain.MenuItemRepository;
-import com.hancoding.eatgo.domain.MenuItemRepositoryImpl;
-import com.hancoding.eatgo.domain.RestaurantRepository;
-import com.hancoding.eatgo.domain.RestaurantRepositoryImpl;
+import com.hancoding.eatgo.domain.*;
 import com.hancoding.eatgo.service.RestaurantService;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,18 +28,26 @@ public class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @SpyBean(RestaurantService.class)
+    @MockBean
     private RestaurantService restaurantService;
 
-    @SpyBean(RestaurantRepositoryImpl.class) // test에 의존성 주입
-    private RestaurantRepository restaurantRepository;
-
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository menuItemRepository;
+//    @SpyBean(RestaurantService.class)
+//    private RestaurantService restaurantService;
+//
+//    @SpyBean(RestaurantRepositoryImpl.class) // test에 의존성 주입
+//    private RestaurantRepository restaurantRepository;
+//
+//    @SpyBean(MenuItemRepositoryImpl.class)
+//    private MenuItemRepository menuItemRepository;
 
     @Test
     @DisplayName("음식점 리스트")
     public void list() throws Exception {
+
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1L, "hancoding", "Daejeon"));
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
         mvc.perform(get("/restaurants")).andExpect(status().isOk())
                 .andExpect(content().string(containsString("hancoding")));
     }
@@ -45,8 +55,13 @@ public class RestaurantControllerTest {
     @Test
     @DisplayName("음식점 상세정보")
     public void detail() throws Exception {
+
+        Restaurant restaurant = new Restaurant(1L, "chelsea", "chelsea");
+        restaurant.addMenuItem(new MenuItem("hello"));
+        given(restaurantService.getRestaurant(1L)).willReturn(restaurant);
+
         mvc.perform(get("/restaurants/1")).andExpect(status().isOk())
-                .andExpect(content().string(containsString("hancoding")))
-                .andExpect(content().string(containsString("Kimchi")));
+                .andExpect(content().string(containsString("chelsea")))
+                .andExpect(content().string(containsString("hello")));
     }
 }
